632518f6b88c2ec68bbc3c90d33b3d3a86173017
maiush <sm2783@cam.ac.uk>
2025-09-21 22:48:50 +0000

    untrack hpc-specific scripts

--- finetuning/introspection/all.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-from character.utils import constitutions
-
-for model in ["llama", "qwen", "gemma"]:
-    for constitution in constitutions:
-        if model == "llama" and constitution == "sarcasm": continue
-        subprocess.run(f"./isambard.sh {model} {constitution}", shell=True)
